f84e2b5bdfa2b9cb875a93404dc3227aa63ad95b
Hsueh <archsueh@gmail.com>
2026-06-19 10:42:58 +0800

    build(git): integrate swift package resolve check in pre-commit hook

--- scripts/git-pre-commit.sh
+++ scripts/git-pre-commit.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # Pre-commit hook to run tests before committing in archer
+echo "Checking Swift Package dependencies resolution..."
+swift package resolve
+if [ $? -ne 0 ]; then
+    echo "ERROR: Swift Package dependencies resolution failed. Commit aborted."
+    exit 1
+fi
+
 echo "Running swift test before committing..."
 swift test
 if [ $? -ne 0 ]; then
     echo "ERROR: swift test failed. Commit aborted."
     exit 1
 fi
+
